dae1eeb3599906fe0251b9a7dbecc9ee169ed1ce
Peter van Dommelen <pvandommelen@users.noreply.github.com>
2025-12-01 17:20:48 +0100

    Day 1

--- src/day/day01.rs
+++ src/day/day01.rs
@@ -1,15 +1,93 @@
-type Prepared = Vec<u64>;
+use winnow::Parser;
+use winnow::ascii::dec_uint;
+use winnow::combinator::alt;
+
+#[derive(Debug, Copy, Clone)]
+pub enum Direction {
+    Left,
+    Right,
+}
+
+#[derive(Debug, Copy, Clone)]
+struct Instruction {
+    direction: Direction,
+    amount: u32,
+}
+
+type Prepared = Vec<Instruction>;
 
 fn prepare(input: &str) -> Prepared {
-    input.lines().map(|l| l.parse().unwrap()).collect()
+    input
+        .lines()
+        .map(|l| {
+            (
+                alt(("L".value(Direction::Left), "R".value(Direction::Right))),
+                dec_uint::<_, _, ()>,
+            )
+                .map(|(direction, amount)| Instruction { direction, amount })
+                .parse(l)
+                .unwrap()
+        })
+        .collect()
 }
 
-fn p1(input: &Prepared) -> u64 {
-    input.iter().sum()
+fn p1(input: &Prepared) -> u32 {
+    let mut position: u32 = 50;
+    let mut times_pointed_at_zero = 0;
+    for instruction in input {
+        let amount = instruction.amount % 100;
+        match instruction.direction {
+            Direction::Left => {
+                if position < amount {
+                    position += 100;
+                }
+                position -= amount;
+            }
+            Direction::Right => {
+                position += amount;
+                if position >= 100 {
+                    position -= 100;
+                }
+            }
+        }
+        if position == 0 {
+            times_pointed_at_zero += 1;
+        }
+    }
+    times_pointed_at_zero
 }
 
-fn p2(input: &Prepared) -> u64 {
-    input.iter().sum()
+fn p2(input: &Prepared) -> u32 {
+    let mut position: u32 = 50;
+    let mut times_pointed_at_zero = 0;
+    for instruction in input {
+        times_pointed_at_zero += instruction.amount / 100;
+        let amount = instruction.amount % 100;
+        match instruction.direction {
+            Direction::Left => {
+                if position < amount {
+                    if position != 0 {
+                        times_pointed_at_zero += 1;
+                    }
+                    position += 100;
+                }
+                position -= amount;
+            }
+            Direction::Right => {
+                position += amount;
+                if position >= 100 {
+                    position -= 100;
+                    if position != 0 {
+                        times_pointed_at_zero += 1;
+                    }
+                }
+            }
+        }
+        if position == 0 {
+            times_pointed_at_zero += 1;
+        }
+    }
+    times_pointed_at_zero
 }
 
 crate::register!(SOLVER, 1, |ctx, input| {
@@ -25,15 +103,24 @@ crate::register!(SOLVER, 1, |ctx, input| {
 mod tests {
     use super::*;
 
-    const EXAMPLE_INPUT: &str = "";
+    const EXAMPLE_INPUT: &str = "L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82";
 
     #[test]
     fn example_part1() {
-        assert_eq!(p1(&prepare(EXAMPLE_INPUT)), 0);
+        assert_eq!(p1(&prepare(EXAMPLE_INPUT)), 3);
     }
 
     #[test]
     fn example_part2() {
-        assert_eq!(p2(&prepare(EXAMPLE_INPUT)), 0);
+        assert_eq!(p2(&prepare(EXAMPLE_INPUT)), 6);
     }
 }
