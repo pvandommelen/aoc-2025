8dcc20799e62b426bf36ca7e52436152c552c656
Peter van Dommelen <pvandommelen@users.noreply.github.com>
2025-12-02 19:18:07 +0100

    Day 2 opt

--- src/day/day02.rs
+++ src/day/day02.rs
@@ -9,23 +9,40 @@ fn prepare(input: &str) -> Prepared {
     input
         .split(',')
         .map(|l| {
-            separated_pair(dec_uint::<_, _, ()>, '-', dec_uint)
+            separated_pair(dec_uint::<_, u64, ()>, '-', dec_uint)
                 .map(|(a, b)| a..=b)
                 .parse(l)
                 .unwrap()
+        })
+        // Make sure each range entry has the same number of digits as start and end.
+        .flat_map(|range| {
+            let mut next_start = *range.start();
+            let mut ranges = vec![];
+            for log in range.start().ilog10()..range.end().ilog10() {
+                let boundary = 10u64.pow(log + 1);
+                ranges.push(next_start..=(boundary - 1));
+                next_start = boundary;
+            }
+            ranges.push(next_start..=*range.end());
+            ranges
+        })
+        // Assert previous step.
+        .inspect(|range| {
+            let a = range.start().ilog10();
+            let b = range.end().ilog10();
+            assert_eq!(a, b);
         })
         .collect()
 }
 
 fn p1(input: &Prepared) -> u64 {
-    fn valid(num: u64) -> bool {
-        let s = num.to_string();
-        if s.len() % 2 == 1 {
-            // odd length, can't be two numbers repeated twice.
-            return true;
-        }
+    /// Check if the range is entirely valid by checking if the length is odd.
+    fn range_valid(range: &RangeInclusive<u64>) -> bool {
+        range.start().ilog10() % 2 == 0
+    }
 
-        if s.as_bytes()[..s.len() / 2] == s.as_bytes()[s.len() / 2..] {
+    fn valid(num: u64, factor: u64) -> bool {
+        if num / factor == num % factor {
             return false;
         }
 
@@ -34,27 +51,37 @@ fn p1(input: &Prepared) -> u64 {
 
     input
         .iter()
-        .flat_map(|range| range.clone().into_iter())
-        .filter(|num| !valid(*num))
+        .filter(|range| !range_valid(range))
+        .flat_map(|range| {
+            let log = range.start().ilog10();
+            let factor = 10u64.pow((log + 1) / 2);
+            range
+                .clone()
+                .into_iter()
+                .filter(move |num| !valid(*num, factor))
+        })
         .sum()
 }
 
 fn p2(input: &Prepared) -> u64 {
     fn valid(num: u64) -> bool {
-        let s = num.to_string();
-        let b = s.as_bytes();
+        let len = num.ilog10() as usize + 1;
 
-        'next_slice_length: for slice_length in 1..=b.len() / 2 {
-            if b.len() % slice_length != 0 {
+        'next_slice_length: for slice_length in 1..=len / 2 {
+            if len % slice_length != 0 {
                 // odd length, can't be numbers repeated with length slice_length.
                 continue;
             }
 
-            let slice = &b[..slice_length];
-            for i in (slice_length..b.len()).step_by(slice_length) {
-                if slice != &b[i..i + slice_length] {
+            let factor = 10u64.pow(slice_length as u32);
+
+            let slice = num % factor;
+            let mut remainder = num / factor;
+            while remainder != 0 {
+                if slice != remainder % factor {
                     continue 'next_slice_length;
                 }
+                remainder /= factor;
             }
             return false;
         }
